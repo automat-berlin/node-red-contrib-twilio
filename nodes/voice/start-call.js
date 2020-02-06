d5d48d61c4ba72966a35aa3d24b34d8ae67d98b5
raphqu <raphqu@gmail.com>
2020-02-06 09:12:00 +0100

    feat: Create webhook endpoint inside start-call node

--- nodes/voice/start-call.js
+++ nodes/voice/start-call.js
@@ -1,33 +1,60 @@
 module.exports = function(RED) {
   'use strict';
   var url = require('url');
+  var bodyParser = require('body-parser');
 
   function StartCallNode(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
     this.account = RED.nodes.getNode(config.account);
+    this.from = config.from;
+    this.to = config.to;
+    this.webhookUrl = '/webhook-' + randomId();
+    this.method = 'post';
+    var node = this;
+
     if (!node.account) {
       this.warn('missing account configuration');
       return;
     }
-    if (!config.url) {
-      this.warn('missing path');
-      return;
+
+    function randomId() {
+      return Math.floor(Math.random() * 1000000);
     }
-    this.url = config.url;
-    if (this.url[0] !== '/') {
-      this.url = '/' + this.url;
-    }
-    this.from = config.from;
-    this.to = config.to;
+
+    this.errorHandler = function(err, req, res) {
+      node.warn(err);
+      res.sendStatus(500);
+    };
+
+    this.callback = function(req, res) {
+      var msgid = RED.util.generateId();
+      res._msgid = msgid;
+      var msg = { _msgid: msgid, req: req, res: { _res: res }, payload: req.body };
+      node.send(msg);
+    };
+
+    var maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+    var urlencParser = bodyParser.urlencoded({ limit: maxApiRequestSize, extended: true });
+
+    RED.httpNode.post(this.webhookUrl, urlencParser, this.callback, this.errorHandler);
+
+    this.on('close', function() {
+      var node = this;
+      RED.httpNode._router.stack.forEach(function(route, i, routes) {
+        if (route.route && route.route.path === node.webhookUrl && route.route.methods[node.method]) {
+          routes.splice(i, 1);
+        }
+      });
+    });
+
     var client = require('twilio')(node.account.credentials.sid, node.account.credentials.token);
     client.api.baseUrl = node.account.credentials.apiUrl;
 
     node.on('input', function(msg) {
-      var absoluteUrl = url.resolve(this.context().global.get('baseUrl'), node.url);
+      var absoluteWebhookUrl = url.resolve(this.context().global.get('baseUrl'), node.webhookUrl);
       client.calls
         .create({
-          url: msg.payload.url || absoluteUrl,
+          url: absoluteWebhookUrl,
           to: msg.payload.to || node.to,
           from: msg.payload.from || node.from,
         })
